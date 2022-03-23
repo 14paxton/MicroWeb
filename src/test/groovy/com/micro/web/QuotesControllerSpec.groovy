81c8058740c655e4b27a81016cb304b21ddd2c34
Brandon Paxton <bpaxton@talentplus.com>
2022-03-23 10:24:20 -0500

    fixed test with custom error

--- src/test/groovy/com/micro/web/QuotesControllerSpec.groovy
+++ src/test/groovy/com/micro/web/QuotesControllerSpec.groovy
@@ -1,8 +1,12 @@
 package com.micro.web
 
 import com.micro.web.broker.model.Quote
+import com.micro.web.broker.store.com.micro.web.broker.error.CustomError
+import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.rxjava3.http.client.Rx3HttpClient
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
@@ -22,6 +26,27 @@ class QuotesControllerSpec extends Specification {
 
     @Inject
     @Client("/") Rx3HttpClient client
+
+
+    void "thrownException"(){
+        when:
+        HttpClientResponseException errorResponse
+        try{
+            client.toBlocking().retrieve(HttpRequest.GET("/quotes/EXCEPTION"), Argument.of(Quote.class) as Argument<Object>, Argument.of(CustomError.class))
+        } catch(HttpClientResponseException e){
+            errorResponse = e
+        }
+
+        then:
+        HttpStatus.NOT_FOUND == errorResponse?.getResponse()?.getStatus()
+        Optional<CustomError> ce  = errorResponse.getResponse().getBody(CustomError.class)
+        CustomError error = ce.get()
+        404 == error.status
+        "NOT_FOUND" == error.error
+        "quote for symbol not available" == error.message
+        "/quote/EXCEPTION" == error.path
+
+    }
 
     void "returnQuotePerSymbol"(){
         when:
