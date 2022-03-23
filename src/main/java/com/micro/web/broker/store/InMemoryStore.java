8aa9821d6c99ae3373bbc3b3ed4477d9e9a94b1a
Brandon Paxton <bpaxton@talentplus.com>
2022-03-23 09:46:35 -0500

    fixed

--- src/main/java/com/micro/web/broker/store/InMemoryStore.java
+++ src/main/java/com/micro/web/broker/store/InMemoryStore.java
@@ -11,28 +11,39 @@ import java.util.stream.*;
 @Singleton
 public class InMemoryStore {
     private final List<Symbol> symbols;
+    private final Map<String, Quote> cachedQuotes = new HashMap<>();
 
     public InMemoryStore() {
-        this.symbols = Stream.of("AAPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA" )
+        this.symbols = Stream.of("AAPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA")
                 .map(Symbol::new)
                 .collect(Collectors.toList());
+        this.symbols.forEach(symbol -> {
+            cachedQuotes.put(symbol.getValue(), randomQuote(symbol));
+        });
     }
 
-
-    public List<Symbol> getAllSymbols() {
-        return symbols;
-    }
-
-    public Quote fetchQuote(final String symbol){
+    private Quote randomQuote(Symbol symbol) {
         return Quote.builder()
-                .symbol(new Symbol(symbol))
+                .symbol(symbol)
                 .bid(randomValue())
                 .ask(randomValue())
                 .lastPrice(randomValue())
                 .build();
     }
 
+    public List<Symbol> getAllSymbols() {
+        return symbols;
+    }
+
+    public Optional<Quote> fetchQuote(final String symbol) {
+        return Optional.ofNullable(cachedQuotes.get(symbol));
+    }
+
     private BigDecimal randomValue() {
-        return BigDecimal.valueOf(ThreadLocalRandom.current().nextDouble(1,100));
+        return BigDecimal.valueOf(ThreadLocalRandom.current().nextDouble(1, 100));
+    }
+
+    public void update(final Quote quote) {
+        cachedQuotes.put(quote.getSymbol().getValue(), quote);
     }
 }
